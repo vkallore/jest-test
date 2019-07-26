d984d27a20f35ffd84a6e863192c5fdf9575ba49
Vaishak Kallore <vaikoovery@gmail.com>
2019-07-26 11:17:37 +0530

    API call to github users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import request from 'services'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      users: [],
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true })
+    request
+      .get('/users', {})
+      .then(json => {
+        const userData = json.data
+        this.setState({ users: userData, loading: false })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  render() {
+    const { users, loading } = this.state
+    const gitHubUsers = users.map(user => {
+      return (
+        <li key={user.id}>
+          {user.login} ({user.id})
+        </li>
+      )
+    })
+    return (
+      <div>
+        <h1>Github Users</h1>
+        {users.length === 0 && loading !== true ? (
+          <div>No Users Found</div>
+        ) : loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ol>{gitHubUsers}</ol>
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
